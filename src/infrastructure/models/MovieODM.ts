8078f6493644473e5f98b199b1d99f79002f50f4
Luiz Calaça <luizcalaca@gmail.com>
2022-10-22 17:13:25 -0300

    feat: creating mongodb model for movie

---
+++ src/infrastructure/models/MovieODM.ts
@@ -0,0 +1,47 @@
+import { isValidObjectId, Model, UpdateQuery, Schema, model } from 'mongoose';
+import { IMovie } from '../../domain/entities/interfaces/IMovie';
+import { IPersistence } from '../../domain/repository/IPersistence';
+
+class MovieODM implements IPersistence {
+  private _schema: Schema;
+  private _model: Model<IMovie>
+
+  constructor() {
+    this._schema = new Schema<IMovie>({
+      type: { type: String,  mnrequired: true },
+      title: { type: String, required: true },
+      cover: { type: String, required: true },
+    });
+    this._model = model('Movie', this._schema);
+  }
+
+  public async create(obj: IMovie): Promise<IMovie> {
+    return this._model.create({ ...obj });
+  }
+
+  public async readOne(_id: string): Promise<IMovie | null> {
+    if (!isValidObjectId(_id)) throw Error('InvalidMongoId');
+    return this._model.findOne({ _id });
+  }
+
+  public async read(): Promise<IMovie[]> {
+    return this._model.find();
+  }
+
+  public async update(_id: string, obj: Partial<IMovie>):
+    Promise<IMovie | null> {
+    if (!isValidObjectId(_id)) throw Error('Erro');
+
+    return this._model.findByIdAndUpdate(
+      { _id },
+      { ...obj } as UpdateQuery<IMovie>,
+      { new: true },
+    );
+  }
+
+  public async delete(_id: string): Promise<IMovie | null> {
+    if (!isValidObjectId(_id)) throw Error('InvalidMongoId');
+    return this._model.findOneAndDelete({ _id });
+  }
+}
+export default MovieODM;
