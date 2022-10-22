f9a3a7e5ff68a78dc1f558158c7230a352c65c40
Luiz Calaça <luizcalaca@gmail.com>
2022-10-22 17:18:39 -0300

    feat: creating a factory for movie

---
+++ src/infrastructure/factories/MovieFactory.ts
@@ -0,0 +1,12 @@
+import { IPersistence } from '../../domain/repository/IPersistence';
+import { MovieRepository } from '../../domain/repository/MovieRepository';
+import MovieUseCase from '../../domain/usecase/MovieService';
+import MovieController from '../controllers/MovieController';
+import MovieODM from '../models/MovieODM';
+
+const ipersitence: IPersistence = new MovieODM()
+const movieRepository = new MovieRepository(ipersitence)
+const usecase = new MovieUseCase(movieRepository)
+const controller = new MovieController(usecase)
+
+export {controller}
