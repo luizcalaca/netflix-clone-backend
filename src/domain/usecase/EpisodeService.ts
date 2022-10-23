c4f70350dcc167de362c7c5882e22cc0896ac755
Luiz Calaça <luizcalaca@gmail.com>
2022-10-23 16:13:21 -0300

    feat: adding use cases focus on save two entities together if necessary

--- src/domain/usecase/EpisodeService.ts
+++ src/domain/usecase/EpisodeService.ts
@@ -1,12 +1,18 @@
 import { Episode } from "../entities/Episode";
+import { Season } from "../entities/Season";
 import { EpisodeRepository } from "../repository/EpisodeRepository";
 
 class EpisodeUseCase {
 
   constructor(private repository: EpisodeRepository) {}
 
-  public create = async (entity: Omit<Episode, "_id">): Promise<Episode> => {
-    return await this.repository.create(entity)
+  public create = async (entityEpisode: Omit<Episode, "_id">, 
+  entitySeason: Omit<Season, "_id">): Promise<Season> => {
+    return await this.repository.create(entityEpisode, entitySeason)
+  }
+
+  public createEpisode = async (entity: Omit<Episode, "_id">): Promise<Episode> => {
+    return await this.repository.createEpisode(entity)
   }
 
   public read = async (): Promise<Episode> => {
--- src/domain/usecase/SeasonService.ts
+++ src/domain/usecase/SeasonService.ts
@@ -1,3 +1,4 @@
+import { Movie } from "../entities/Movie";
 import { Season } from "../entities/Season";
 import { SeasonRepository } from "../repository/SeasonRepository";
 
@@ -5,9 +6,14 @@ class SeasonUseCase {
 
   constructor(private repository: SeasonRepository) {}
 
-  public create = async (entity: Omit<Season, "_id">): Promise<Season> => {
-    return await this.repository.create(entity)
+  public create = async (entitySeason: Omit<Season, "_id">,
+    entityMovie:Omit<Movie, "_id">): Promise<Season> => {
+      return await this.repository.create(entitySeason, entityMovie)
   }
+
+  public createSeason = async (entitySeason: Omit<Season, "_id">) => {
+    return await this.repository.createSeason(entitySeason)
+}
 
   public read = async (): Promise<Season> => {
     return await this.repository.read()
