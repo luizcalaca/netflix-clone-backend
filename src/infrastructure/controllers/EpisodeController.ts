578e31fc4721d253f7423d0f2519bd78b1b2f3d7
Luiz Calaça <luizcalaca@gmail.com>
2022-10-23 12:55:39 -0300

    feat: creating a controller for episode

---
+++ src/infrastructure/controllers/EpisodeController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express';
+import { Episode } from '../../domain/entities/Episode';
+import { IEpisode } from '../../domain/entities/interfaces/IEpisode';
+import EpisodeUseCase from '../../domain/usecase/EpisodeService';
+
+class EpisodeController {
+
+  constructor(private EpisodeUseCase: EpisodeUseCase) {}
+
+  public create = async (req: Request, res: Response) => {
+    const episode: Omit<Episode, "_id"> = {
+        season_id: req.body.season_id,
+        title: req.body.title,
+        description: req.body.description,
+        number: req.body.number,
+        cover: req.body.cover,
+    }
+
+    const result = await this.EpisodeUseCase.create(episode)
+    return res.status(201).json(result);
+  }
+
+  public list = async (req: Request, res: Response) => {
+    const result = await this.EpisodeUseCase.read()
+    return res.status(201).json(result);
+  }
+}
+export default EpisodeController;
