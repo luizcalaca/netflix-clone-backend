e752829966ac81b0fb765eaee9f72105bbcf7c21
Luiz Calaça <luizcalaca@gmail.com>
2022-10-23 12:56:59 -0300

    feat: creating a route for episode on main

--- src/application/app.ts
+++ src/application/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { ErrorHandler } from '../infrastructure/middlewares/Error';
+import episodeRoutes from '../infrastructure/routes/episode.routes';
 import movieRoutes from '../infrastructure/routes/movie.routes';
 import seasonRoutes from '../infrastructure/routes/season.routes';
 import userRoutes from '../infrastructure/routes/user.routes';
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use('/users', userRoutes);
 app.use('/movies', movieRoutes);
 app.use('/seasons', seasonRoutes);
+app.use('/episodes', episodeRoutes);
 app.use(ErrorHandler.execute)
 
 export default app;
