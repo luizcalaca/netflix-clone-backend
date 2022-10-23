139437e56c437177bf5ed3547ea87e0d7943a7d8
Luiz Calaça <luizcalaca@gmail.com>
2022-10-23 14:09:35 -0300

    chore: adding helmet and cors for security

--- src/application/app.ts
+++ src/application/app.ts
@@ -4,9 +4,13 @@ import episodeRoutes from '../infrastructure/routes/episode.routes';
 import movieRoutes from '../infrastructure/routes/movie.routes';
 import seasonRoutes from '../infrastructure/routes/season.routes';
 import userRoutes from '../infrastructure/routes/user.routes';
+import cors from 'cors';
+import helmet from 'helmet';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+app.use(helmet());
 app.use('/users', userRoutes);
 app.use('/movies', movieRoutes);
 app.use('/seasons', seasonRoutes);
