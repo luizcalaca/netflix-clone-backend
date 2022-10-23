58d8d8aa2691506c37f6c445b97f2bbad640c078
Luiz Calaça <luizcalaca@gmail.com>
2022-10-23 12:54:10 -0300

    feat: creating entity and interface episode

---
+++ src/domain/entities/Episode.ts
@@ -0,0 +1,10 @@
+class Episode {
+    _id!: string
+    season_id!: string
+    title!: string
+    description!: string
+    number!: string
+    cover!: string
+}
+
+export { Episode }
---
+++ src/domain/entities/interfaces/IEpisode.ts
@@ -0,0 +1,9 @@
+interface IEpisode {
+    season_id: string
+    title: string
+    description: string
+    number: number
+    cover: string
+}
+
+export { IEpisode }
