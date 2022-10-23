f9a8a8a32f97233a2cdaa278d017382a0e0fd8f3
Luiz Calaça <luizcalaca@gmail.com>
2022-10-23 13:11:01 -0300

    chore: connection on utils

--- src/application/server.ts
+++ src/application/server.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import app from './app';
-import connectToDatabase from '../infrastructure/models/Connection';
+import connectToDatabase from '../infrastructure/utils/Connection';
 
 const PORT = process.env.PORT || 3001;
 connectToDatabase()
