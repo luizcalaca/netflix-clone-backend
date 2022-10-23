29247d78e7a899acdee640bb96bdec6edd60f44c
Luiz Calaça <luizcalaca@gmail.com>
2022-10-23 00:13:28 -0300

    feat: creating a factory for season

---
+++ src/infrastructure/factories/SeasonFactory.ts
@@ -0,0 +1,12 @@
+import { IPersistence } from '../../domain/repository/IPersistence';
+import { SeasonRepository } from '../../domain/repository/SeasonRepository';
+import SeasonUseCase from '../../domain/usecase/SeasonService';
+import SeasonController from '../controllers/SeasonController';
+import SeasonODM from '../models/SeasonODM';
+
+const ipersitence: IPersistence = new SeasonODM()
+const seasonRepository = new SeasonRepository(ipersitence)
+const usecase = new SeasonUseCase(seasonRepository)
+const controller = new SeasonController(usecase)
+
+export {controller}
