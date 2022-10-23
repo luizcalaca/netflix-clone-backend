03d4e22a9e23c641fd28ea7b7c19145b1fd6d7d8
Luiz Calaça <luizcalaca@gmail.com>
2022-10-23 00:12:41 -0300

    feat: creating mongodb model for season

---
+++ src/infrastructure/models/SeasonODM.ts
@@ -0,0 +1,46 @@
+import { isValidObjectId, Model, UpdateQuery, Schema, model } from 'mongoose';
+import { ISeason } from '../../domain/entities/interfaces/ISeason';
+import { IPersistence } from '../../domain/repository/IPersistence';
+
+class SeasonODM implements IPersistence {
+  private _schema: Schema;
+  private _model: Model<ISeason>
+
+  constructor() {
+    this._schema = new Schema<ISeason>({
+      movie_id: { type: Number,  required: true, ref: 'Movie' },
+      title: { type: String, required: true },
+    });
+    this._model = model('Season', this._schema);
+  }
+
+  public async create(obj: ISeason): Promise<ISeason> {
+    return this._model.create({ ...obj });
+  }
+
+  public async readOne(_id: string): Promise<ISeason | null> {
+    if (!isValidObjectId(_id)) throw Error('InvalidMongoId');
+    return this._model.findOne({ _id });
+  }
+
+  public async read(): Promise<ISeason[]> {
+    return this._model.find();
+  }
+
+  public async update(_id: string, obj: Partial<ISeason>):
+    Promise<ISeason | null> {
+    if (!isValidObjectId(_id)) throw Error('Erro');
+
+    return this._model.findByIdAndUpdate(
+      { _id },
+      { ...obj } as UpdateQuery<ISeason>,
+      { new: true },
+    );
+  }
+
+  public async delete(_id: string): Promise<ISeason | null> {
+    if (!isValidObjectId(_id)) throw Error('InvalidMongoId');
+    return this._model.findOneAndDelete({ _id });
+  }
+}
+export default SeasonODM;
