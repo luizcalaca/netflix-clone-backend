22979e597cff93c789bb7666bfc7cde25cf2d74d
Luiz Calaça <luizcalaca@gmail.com>
2022-10-23 00:10:27 -0300

    tests: BDD/TDD for season's creation

---
+++ tests/bdd/season.create.spec.ts
@@ -0,0 +1,38 @@
+import { SeasonRepository } from "../../src/domain/repository/SeasonRepository"
+import { stubInterface } from "ts-sinon";
+import chaiAsPromised from 'chai-as-promised';
+import * as chai from 'chai'
+import sinon from 'sinon'
+import SeasonService from "../../src/domain/usecase/SeasonService";
+import { ISeason } from "../../src/domain/entities/interfaces/ISeason";
+import { Season } from "../../src/domain/entities/Season";
+import { IPersistence } from "../../src/domain/repository/IPersistence";
+
+chai.use(chaiAsPromised)
+const expect = chai.expect
+
+describe('BDD - Creating an Season', () => {
+    it('BDD - Should create an Season', async () => {
+        const season: Omit<Season, "_id"> = {
+            movie_id: 2,
+            title: 'the back again once time repeat',
+        }
+
+        const seasonMock: Season = {
+            _id: "5ce819935e539c343f141ece",
+            movie_id: 2,
+            title: 'the back again once time repeat',
+        }
+
+        const iPersistence = stubInterface<IPersistence>()
+        const seasonRepository = new SeasonRepository(iPersistence)
+        seasonRepository.create = sinon.stub().returns(seasonMock)
+
+        const usecase = new SeasonService(seasonRepository)
+        usecase.create = sinon.stub().returns(seasonMock)
+
+        const result = usecase.create(season)
+
+        expect(result).to.be.equal(seasonMock)
+    })
+})
