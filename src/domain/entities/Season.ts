5802a1a8a9467da1f4586d71ae3f2e464b2f098c
Luiz Calaça <luizcalaca@gmail.com>
2022-10-23 00:11:08 -0300

    feat: creating entities for season and interface

---
+++ src/domain/entities/Season.ts
@@ -0,0 +1,7 @@
+class Season {
+    _id!: string
+    movie_id!: number
+    title!: string
+}
+
+export { Season }
---
+++ src/domain/entities/interfaces/ISeason.ts
@@ -0,0 +1,6 @@
+interface ISeason {
+    movie_id: number
+    title: string
+}
+
+export { ISeason }
