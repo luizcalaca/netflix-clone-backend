06cf740c26a0a68f20e4b5365342b1b47f135c94
Luiz Calaça <luizcalaca@gmail.com>
2022-10-23 00:14:12 -0300

    feat: creating routes on main app.ts

--- src/application/app.ts
+++ src/application/app.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import { ErrorHandler } from '../infrastructure/middlewares/Error';
 import movieRoutes from '../infrastructure/routes/movie.routes';
+import seasonRoutes from '../infrastructure/routes/season.routes';
 import userRoutes from '../infrastructure/routes/user.routes';
 
 const app = express();
 app.use(express.json());
 app.use('/users', userRoutes);
 app.use('/movies', movieRoutes);
+app.use('/seasons', seasonRoutes);
 app.use(ErrorHandler.execute)
 
 export default app;
