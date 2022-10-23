4132208af1329332e1f8c7d4f1e2209870c6899d
Luiz Calaça <luizcalaca@gmail.com>
2022-10-23 14:11:53 -0300

    chore: adding morgan for logs

--- src/application/app.ts
+++ src/application/app.ts
@@ -6,11 +6,13 @@ import seasonRoutes from '../infrastructure/routes/season.routes';
 import userRoutes from '../infrastructure/routes/user.routes';
 import cors from 'cors';
 import helmet from 'helmet';
+import morgan from 'morgan';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
+app.use(morgan('dev'));
 app.use('/users', userRoutes);
 app.use('/movies', movieRoutes);
 app.use('/seasons', seasonRoutes);
