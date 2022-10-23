ac8a12e6975c4330422c52b9afc6ec157d47797c
Luiz Calaça <luizcalaca@gmail.com>
2022-10-23 12:56:09 -0300

    feat: creating a factory for episode

---
+++ src/infrastructure/factories/EpisodeFactory.ts
@@ -0,0 +1,12 @@
+import { IPersistence } from '../../domain/repository/IPersistence';
+import { EpisodeRepository } from '../../domain/repository/EpisodeRepository';
+import EpisodeUseCase from '../../domain/usecase/EpisodeService';
+import EpisodeController from '../controllers/EpisodeController';
+import EpisodeODM from '../models/EpisodeODM';
+
+const ipersitence: IPersistence = new EpisodeODM()
+const episodeRepository = new EpisodeRepository(ipersitence)
+const usecase = new EpisodeUseCase(episodeRepository)
+const controller = new EpisodeController(usecase)
+
+export {controller}
